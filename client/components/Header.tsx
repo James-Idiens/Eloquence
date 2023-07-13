9ba2a5c5256473aacc57074218337c5fceb1f099
James Idiens <jidiens@outlook.com>
2023-07-13 17:18:36 +1200

    switched header to tailwind css

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,22 +2,23 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className="header-container">
+    <div className="flex items-center justify-between py-0 px-6">
       <img
         src="server/Public/headerImage.webp"
         alt="logo"
-        className="header-logo"
+        className="h-16 sm:h-16 md:h-32 lg:h-48 -ml-2 -my-10"
       />
-
-      <Link to="/about" className="header-about-us">
-        About Us
-      </Link>
-      <Link to="/faq" className="header-faq">
-        FAQs
-      </Link>
-      <Link to="/login" className="header-login">
-        Login
-      </Link>
+      <div className="flex space-x-8">
+        <Link to="/about" className="text-white text-4xl">
+          About Us
+        </Link>
+        <Link to="/faq" className="text-white text-4xl ml-4">
+          FAQs
+        </Link>
+        <Link to="/login" className="text-white text-4xl">
+          Login
+        </Link>
+      </div>
     </div>
   )
 }
