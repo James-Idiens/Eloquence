c5ca995def33d20c6763b4e6ce9bd41f85064018
James Idiens <jidiens@outlook.com>
2023-07-21 11:18:39 +1200

    chapters can be updated

--- client/apis/chapters.ts
+++ client/apis/chapters.ts
@@ -7,3 +7,24 @@ export async function createChapter(novelId: number, chapterData: ChapterData) {
     .send(chapterData)
   return response.body
 }
+
+export async function getChapterById(novelId: number, chapterId: number) {
+  const response = await request.get(
+    `/api/v1/novels/${novelId}/chapters/${chapterId}`
+  )
+  return response.body
+}
+
+export async function updateChapter(
+  novelId: number,
+  chapterId: number,
+  updatedChapterData: ChapterData
+) {
+  await request
+    .put(`/api/v1/novels/${novelId}/chapters/${chapterId}`)
+    .send(updatedChapterData)
+}
+
+export async function deleteChapter(novelId: number, chapterId: number) {
+  await request.delete(`/api/v1/novels/${novelId}/chapters/${chapterId}`)
+}
--- server/routes/chapter-routes.ts
+++ server/routes/chapter-routes.ts
@@ -4,7 +4,7 @@ import {
   createChapter,
   deleteChapter,
   updateChapter,
-  getChapterByID,
+  getChapterById,
 } from '../db/controllers/chaptersController'
 
 const router = express.Router()
@@ -18,6 +18,43 @@ router.post('/:id/chapters', async (req, res) => {
   } catch (error) {
     console.error(error)
     res.status(500).send('Failed to create chapter')
+  }
+})
+
+router.get('/:id/chapters/:chapterId', async (req, res) => {
+  try {
+    const novelId = Number(req.params.id)
+    const chapterId = Number(req.params.chapterId)
+    const chapter = await getChapterById(novelId, chapterId)
+    res.status(200).json(chapter)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Failed to get chapter')
+  }
+})
+
+router.put('/:id/chapters/:chapterId', async (req, res) => {
+  try {
+    const novelId = Number(req.params.id)
+    const chapterId = Number(req.params.chapterId)
+    const updatedChapterData = req.body
+    await updateChapter(novelId, chapterId, updatedChapterData)
+    res.status(200).send('Chapter updated')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Failed to update chapter')
+  }
+})
+
+router.delete('/:id/chapters/:chapterId', async (req, res) => {
+  try {
+    const novelId = Number(req.params.id)
+    const chapterId = Number(req.params.chapterId)
+    await deleteChapter(novelId, chapterId)
+    res.status(200).send('Chapter deleted')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Failed to delete chapter')
   }
 })
 
